35f2e48b79006e0cf7a4aca464c4d6cc6fbd86f9
IamSoPradaYou <64264723+IamSoPrada@users.noreply.github.com>
2020-10-01 00:23:23 +0300

    Update index.js
    Added cors variable

--- livechat/server/index.js
+++ livechat/server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const socketio = require("socket.io");
 const http = require("http");
+const cors = require("cors");
 
 const { addUser, removeUser, getUser, getUsersInRoom } = require("./users.js");
 
@@ -10,6 +11,9 @@ const router = require("./router");
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+app.use(router)
+app.use(cors())
 
 io.on("connection", (socket) => {
     socket.on("join", ({ name, room }, callback) => {
