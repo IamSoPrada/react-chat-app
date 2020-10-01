f44ca9ebbc61c0e7f14174a3271b8356e4b3be7a
IamSoPradaYou <64264723+IamSoPrada@users.noreply.github.com>
2020-10-01 22:18:01 +0300

    32 commit

--- livechat/client/src/components/Chat/Chat.js
+++ livechat/client/src/components/Chat/Chat.js
@@ -15,7 +15,7 @@ const Chat = ({ location }) => {
     const [room, setRoom] = useState("");
     const [message, setMessage] = useState("");
     const [messages, setMessages] = useState([]);
-    const ENDPOINT = "localhost:5000";
+    const ENDPOINT = "http://realtime-socketio-app.herokuapp.com/";
     useEffect(() => {
         const { name, room } = queryString.parse(location.search);
 
